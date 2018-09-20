3682d1beda021c46b3276fcdd5e0b8271d26e279
jamespferguson <ncjamesferguson@gmail.com>
2018-09-20 12:06:15 -0400

    add Big O example

--- Java/Otest.java
+++ Java/Otest.java
@@ -1,3 +1,41 @@
 public class Otest {
 
+    public static int loopCount = 0;
+    public static int nestedLoopCount = 0;
+    public static int tripleCount = 0;
+
+    public static void main(String[] args) {
+        // This section is O(n)
+        for (int i = 0; i < 10; i++) {
+            loopCount++;
+        }
+        for (int i = 0; i < 10; i++) {
+            loopCount++;
+        }
+        System.out.println(loopCount);
+
+        // This section is O(n^2)
+        for (int i = 0; i < 10; i++) {
+            nestedLoopCount++;
+            for (int j = 0; j < 10; j++) {
+                nestedLoopCount++;
+            }
+        }
+        System.out.println(nestedLoopCount);
+
+        // This section is O(n^3)
+        for (int i = 0; i < 10; i++) {
+            tripleCount++;
+            for (int j = 0; j < 10; j++) {
+                tripleCount++;
+                for (int k = 0; k < 10; k++) {
+                    tripleCount++;
+                }
+            }
+        }
+
+        System.out.println(tripleCount);
+
+    }
+
 }
